d273a07cb4237fd02d12d21e569d3f8cd0de0484
cghost182 <cristian182@gmail.com>
2019-06-11 22:49:12 -0500

    Initial project

---
+++ ChrisPelis/ChrisPelis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChrisPelis
+//
+//  Created by Christian Collazos on 6/11/19.
+//  Copyright © 2019 Christian Collazos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
